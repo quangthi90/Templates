0bc0b7f003ab37b6f20caedb20ca787eb41d6718
Bommer <quangthi_90@yahoo.com.vn>
2014-10-05 19:25:59 +0700

    - Add backup config file
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ backup/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Templates/admin/');
+define('HTTP_CATALOG', 'http://localhost/Templates/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Templates/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Templates/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Templates/admin/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Templates/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Templates/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Templates/admin/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Templates/admin/view/template/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Templates/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Templates/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Templates/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Templates/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Templates/system/logs/');
+define('DIR_CATALOG', 'F:\Program\xampp\htdocs\Templates/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'templates');
+define('DB_PREFIX', '');
+?>
