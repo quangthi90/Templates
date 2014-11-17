62eff26f6fa2eeb2cbdd4a3404f574f4ef7c74fe
Bommer <quangthi_90@yahoo.com.vn>
2014-11-17 15:02:08 +0700

    - Add Salary type management
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ admin/model/salary/type.php
@@ -0,0 +1,304 @@
+<?php
+class ModelSalaryType extends Model {
+	public function addtype($data) {
+		$this->db->query("INSERT INTO " . DB_PREFIX . "type SET parent_id = '" . (int)$data['parent_id'] . "', `top` = '" . (isset($data['top']) ? (int)$data['top'] : 0) . "', `column` = '" . (int)$data['column'] . "', sort_order = '" . (int)$data['sort_order'] . "', status = '" . (int)$data['status'] . "', date_modified = NOW(), date_added = NOW()");
+
+		$type_id = $this->db->getLastId();
+
+		if (isset($data['image'])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "type SET image = '" . $this->db->escape(html_entity_decode($data['image'], ENT_QUOTES, 'UTF-8')) . "' WHERE type_id = '" . (int)$type_id . "'");
+		}
+
+		foreach ($data['type_description'] as $language_id => $value) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "type_description SET type_id = '" . (int)$type_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', description = '" . $this->db->escape($value['description']) . "'");
+		}
+
+		// MySQL Hierarchical Data Closure Table Pattern
+		$level = 0;
+
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$data['parent_id'] . "' ORDER BY `level` ASC");
+
+		foreach ($query->rows as $result) {
+			$this->db->query("INSERT INTO `" . DB_PREFIX . "type_path` SET `type_id` = '" . (int)$type_id . "', `path_id` = '" . (int)$result['path_id'] . "', `level` = '" . (int)$level . "'");
+
+			$level++;
+		}
+
+		$this->db->query("INSERT INTO `" . DB_PREFIX . "type_path` SET `type_id` = '" . (int)$type_id . "', `path_id` = '" . (int)$type_id . "', `level` = '" . (int)$level . "'");
+
+		if (isset($data['type_filter'])) {
+			foreach ($data['type_filter'] as $filter_id) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "type_filter SET type_id = '" . (int)$type_id . "', filter_id = '" . (int)$filter_id . "'");
+			}
+		}
+
+		if (isset($data['type_store'])) {
+			foreach ($data['type_store'] as $store_id) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "type_to_store SET type_id = '" . (int)$type_id . "', store_id = '" . (int)$store_id . "'");
+			}
+		}
+
+		// Set which layout to use with this type
+		if (isset($data['type_layout'])) {
+			foreach ($data['type_layout'] as $store_id => $layout) {
+				if ($layout['layout_id']) {
+					$this->db->query("INSERT INTO " . DB_PREFIX . "type_to_layout SET type_id = '" . (int)$type_id . "', store_id = '" . (int)$store_id . "', layout_id = '" . (int)$layout['layout_id'] . "'");
+				}
+			}
+		}
+
+		if ($data['keyword']) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'type_id=" . (int)$type_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
+		}
+
+		$this->cache->delete('type');
+	}
+
+	public function edittype($type_id, $data) {
+		$this->db->query("UPDATE " . DB_PREFIX . "type SET parent_id = '" . (int)$data['parent_id'] . "', `top` = '" . (isset($data['top']) ? (int)$data['top'] : 0) . "', `column` = '" . (int)$data['column'] . "', sort_order = '" . (int)$data['sort_order'] . "', status = '" . (int)$data['status'] . "', date_modified = NOW() WHERE type_id = '" . (int)$type_id . "'");
+
+		if (isset($data['image'])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "type SET image = '" . $this->db->escape(html_entity_decode($data['image'], ENT_QUOTES, 'UTF-8')) . "' WHERE type_id = '" . (int)$type_id . "'");
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_description WHERE type_id = '" . (int)$type_id . "'");
+
+		foreach ($data['type_description'] as $language_id => $value) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "type_description SET type_id = '" . (int)$type_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', description = '" . $this->db->escape($value['description']) . "'");
+		}
+
+		// MySQL Hierarchical Data Closure Table Pattern
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE path_id = '" . (int)$type_id . "' ORDER BY level ASC");
+
+		if ($query->rows) {
+			foreach ($query->rows as $type_path) {
+				// Delete the path below the current one
+				$this->db->query("DELETE FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$type_path['type_id'] . "' AND level < '" . (int)$type_path['level'] . "'");
+
+				$path = array();
+
+				// Get the nodes new parents
+				$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$data['parent_id'] . "' ORDER BY level ASC");
+
+				foreach ($query->rows as $result) {
+					$path[] = $result['path_id'];
+				}
+
+				// Get whats left of the nodes current path
+				$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$type_path['type_id'] . "' ORDER BY level ASC");
+
+				foreach ($query->rows as $result) {
+					$path[] = $result['path_id'];
+				}
+
+				// Combine the paths with a new level
+				$level = 0;
+
+				foreach ($path as $path_id) {
+					$this->db->query("REPLACE INTO `" . DB_PREFIX . "type_path` SET type_id = '" . (int)$type_path['type_id'] . "', `path_id` = '" . (int)$path_id . "', level = '" . (int)$level . "'");
+
+					$level++;
+				}
+			}
+		} else {
+			// Delete the path below the current one
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$type_id . "'");
+
+			// Fix for records with no paths
+			$level = 0;
+
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$data['parent_id'] . "' ORDER BY level ASC");
+
+			foreach ($query->rows as $result) {
+				$this->db->query("INSERT INTO `" . DB_PREFIX . "type_path` SET type_id = '" . (int)$type_id . "', `path_id` = '" . (int)$result['path_id'] . "', level = '" . (int)$level . "'");
+
+				$level++;
+			}
+
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "type_path` SET type_id = '" . (int)$type_id . "', `path_id` = '" . (int)$type_id . "', level = '" . (int)$level . "'");
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_filter WHERE type_id = '" . (int)$type_id . "'");
+
+		if (isset($data['type_filter'])) {
+			foreach ($data['type_filter'] as $filter_id) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "type_filter SET type_id = '" . (int)$type_id . "', filter_id = '" . (int)$filter_id . "'");
+			}		
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_to_store WHERE type_id = '" . (int)$type_id . "'");
+
+		if (isset($data['type_store'])) {		
+			foreach ($data['type_store'] as $store_id) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "type_to_store SET type_id = '" . (int)$type_id . "', store_id = '" . (int)$store_id . "'");
+			}
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_to_layout WHERE type_id = '" . (int)$type_id . "'");
+
+		if (isset($data['type_layout'])) {
+			foreach ($data['type_layout'] as $store_id => $layout) {
+				if ($layout['layout_id']) {
+					$this->db->query("INSERT INTO " . DB_PREFIX . "type_to_layout SET type_id = '" . (int)$type_id . "', store_id = '" . (int)$store_id . "', layout_id = '" . (int)$layout['layout_id'] . "'");
+				}
+			}
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "url_alias WHERE query = 'type_id=" . (int)$type_id. "'");
+
+		if ($data['keyword']) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'type_id=" . (int)$type_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
+		}
+
+		$this->cache->delete('type');
+	}
+
+	public function deletetype($type_id) {
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_path WHERE type_id = '" . (int)$type_id . "'");
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type_path WHERE path_id = '" . (int)$type_id . "'");
+
+		foreach ($query->rows as $result) {	
+			$this->deletetype($result['type_id']);
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_description WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_filter WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_to_store WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "type_to_layout WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "product_to_type WHERE type_id = '" . (int)$type_id . "'");
+		$this->db->query("DELETE FROM " . DB_PREFIX . "url_alias WHERE query = 'type_id=" . (int)$type_id . "'");
+
+		$this->cache->delete('type');
+	} 
+
+	// Function to repair any erroneous categories that are not in the type path table.
+	public function repairCategories($parent_id = 0) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type WHERE parent_id = '" . (int)$parent_id . "'");
+
+		foreach ($query->rows as $type) {
+			// Delete the path below the current one
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$type['type_id'] . "'");
+
+			// Fix for records with no paths
+			$level = 0;
+
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "type_path` WHERE type_id = '" . (int)$parent_id . "' ORDER BY level ASC");
+
+			foreach ($query->rows as $result) {
+				$this->db->query("INSERT INTO `" . DB_PREFIX . "type_path` SET type_id = '" . (int)$type['type_id'] . "', `path_id` = '" . (int)$result['path_id'] . "', level = '" . (int)$level . "'");
+
+				$level++;
+			}
+
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "type_path` SET type_id = '" . (int)$type['type_id'] . "', `path_id` = '" . (int)$type['type_id'] . "', level = '" . (int)$level . "'");
+
+			$this->repairCategories($type['type_id']);
+		}
+	}
+
+	public function gettype($type_id) {
+		$query = $this->db->query("SELECT DISTINCT *, (SELECT GROUP_CONCAT(cd1.name ORDER BY level SEPARATOR ' &gt; ') FROM " . DB_PREFIX . "type_path cp LEFT JOIN " . DB_PREFIX . "type_description cd1 ON (cp.path_id = cd1.type_id AND cp.type_id != cp.path_id) WHERE cp.type_id = c.type_id AND cd1.language_id = '" . (int)$this->config->get('config_language_id') . "' GROUP BY cp.type_id) AS path, (SELECT keyword FROM " . DB_PREFIX . "url_alias WHERE query = 'type_id=" . (int)$type_id . "') AS keyword FROM " . DB_PREFIX . "type c LEFT JOIN " . DB_PREFIX . "type_description cd2 ON (c.type_id = cd2.type_id) WHERE c.type_id = '" . (int)$type_id . "' AND cd2.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+
+		return $query->row;
+	} 
+
+	public function getCategories($data) {
+		$sql = "SELECT cp.type_id AS type_id, GROUP_CONCAT(cd1.name ORDER BY cp.level SEPARATOR ' &gt; ') AS name, c.parent_id, c.sort_order FROM " . DB_PREFIX . "type_path cp LEFT JOIN " . DB_PREFIX . "type c ON (cp.path_id = c.type_id) LEFT JOIN " . DB_PREFIX . "type_description cd1 ON (c.type_id = cd1.type_id) LEFT JOIN " . DB_PREFIX . "type_description cd2 ON (cp.type_id = cd2.type_id) WHERE cd1.language_id = '" . (int)$this->config->get('config_language_id') . "' AND cd2.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+
+		if (!empty($data['filter_name'])) {
+			$sql .= " AND cd2.name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+		}
+
+		$sql .= " GROUP BY cp.type_id ORDER BY name";
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}				
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}	
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+
+	public function gettypeDescriptions($type_id) {
+		$type_description_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type_description WHERE type_id = '" . (int)$type_id . "'");
+
+		foreach ($query->rows as $result) {
+			$type_description_data[$result['language_id']] = array(
+				'name'             => $result['name'],
+				'meta_keyword'     => $result['meta_keyword'],
+				'meta_description' => $result['meta_description'],
+				'description'      => $result['description']
+			);
+		}
+
+		return $type_description_data;
+	}	
+
+	public function gettypeFilters($type_id) {
+		$type_filter_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type_filter WHERE type_id = '" . (int)$type_id . "'");
+
+		foreach ($query->rows as $result) {
+			$type_filter_data[] = $result['filter_id'];
+		}
+
+		return $type_filter_data;
+	}
+
+	public function gettypeStores($type_id) {
+		$type_store_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type_to_store WHERE type_id = '" . (int)$type_id . "'");
+
+		foreach ($query->rows as $result) {
+			$type_store_data[] = $result['store_id'];
+		}
+
+		return $type_store_data;
+	}
+
+	public function gettypeLayouts($type_id) {
+		$type_layout_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "type_to_layout WHERE type_id = '" . (int)$type_id . "'");
+
+		foreach ($query->rows as $result) {
+			$type_layout_data[$result['store_id']] = $result['layout_id'];
+		}
+
+		return $type_layout_data;
+	}
+
+	public function getTotalCategories() {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "type");
+
+		return $query->row['total'];
+	}	
+
+	public function getTotalCategoriesByImageId($image_id) {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "type WHERE image_id = '" . (int)$image_id . "'");
+
+		return $query->row['total'];
+	}
+
+	public function getTotalCategoriesByLayoutId($layout_id) {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "type_to_layout WHERE layout_id = '" . (int)$layout_id . "'");
+
+		return $query->row['total'];
+	}		
+}
+?>
---
+++ admin/view/template/salary/salary_list.tpl
@@ -0,0 +1,60 @@
+<?php echo $header; ?>
+<div id="content">
+  <div class="breadcrumb">
+    <?php foreach ($breadcrumbs as $breadcrumb) { ?>
+    <?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
+    <?php } ?>
+  </div>
+  <?php if ($error_warning) { ?>
+  <div class="warning"><?php echo $error_warning; ?></div>
+  <?php } ?>
+  <?php if ($success) { ?>
+  <div class="success"><?php echo $success; ?></div>
+  <?php } ?>
+  <div class="box">
+    <div class="heading">
+      <h1><img src="view/image/type.png" alt="" /> <?php echo $heading_title; ?></h1>
+      <div class="buttons"><a href="<?php echo $insert; ?>" class="button"><?php echo $button_insert; ?></a><a onclick="$('#form').submit();" class="button"><?php echo $button_delete; ?></a></div>
+    </div>
+    <div class="content">
+      <form action="<?php echo $delete; ?>" method="post" enctype="multipart/form-data" id="form">
+        <table class="list">
+          <thead>
+            <tr>
+              <td width="1" style="text-align: center;"><input type="checkbox" onclick="$('input[name*=\'selected\']').attr('checked', this.checked);" /></td>
+              <td class="left"><?php echo $column_code; ?></td>
+              <td class="left"><?php echo $column_name; ?></td>
+              <td class="left"><?php echo $column_order; ?></td>
+              <td class="right"><?php echo $column_action; ?></td>
+            </tr>
+          </thead>
+          <tbody>
+            <?php if ($types) { ?>
+            <?php foreach ($types as $type) { ?>
+            <tr>
+              <td style="text-align: center;"><?php if ($type['selected']) { ?>
+                <input type="checkbox" name="selected[]" value="<?php echo $type['type_id']; ?>" checked="checked" />
+                <?php } else { ?>
+                <input type="checkbox" name="selected[]" value="<?php echo $type['type_id']; ?>" />
+                <?php } ?></td>
+              <td class="left"><?php echo $type['code']; ?></td>
+              <td class="left"><?php echo $type['name']; ?></td>
+              <td class="left"><?php echo $type['order']; ?></td>
+              <td class="right"><?php foreach ($type['action'] as $action) { ?>
+                [ <a href="<?php echo $action['href']; ?>"><?php echo $action['text']; ?></a> ]
+                <?php } ?></td>
+            </tr>
+            <?php } ?>
+            <?php } else { ?>
+            <tr>
+              <td class="center" colspan="10"><?php echo $text_no_results; ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </form>
+      <div class="pagination"><?php echo $pagination; ?></div>
+    </div>
+  </div>
+</div>
+<?php echo $footer; ?>
