0e5aad1c36f4714309d35025b1f3b55a86c6ac66
Bommer <quangthi_90@yahoo.com.vn>
2014-12-07 01:10:36 +0700

    - Update config
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ backup/config-2.0.1.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Templates/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Templates/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Templates/catalog/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Templates/system/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Templates/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Templates/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Templates/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Templates/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Templates/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Templates/system/download/');
+define('DIR_UPLOAD', 'F:\Program\xampp\htdocs\Templates/system/upload/');
+define('DIR_MODIFICATION', 'F:\Program\xampp\htdocs\Templates/system/modification/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Templates/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'templates');
+define('DB_PREFIX', '');
