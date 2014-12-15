fbd5094145f2c6dd42df677579c39ed9c00c02cd
Bommer <quangthi_90@yahoo.com.vn>
2014-12-16 01:21:31 +0700

    - Update config file
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- admin/config.php
+++ admin/config.php
@@ -7,21 +7,22 @@ define('HTTP_CATALOG', 'http://'.HTTP.'/');
 define('HTTP_IMAGE', 'http://'.HTTP.'/image/');
  
 // HTTPS
-define('HTTPS_SERVER', 'http://'.HTTP.'/admin/');
-define('HTTPS_IMAGE', 'http://'.HTTP.'/image/');
+define('HTTPS_SERVER', 'https://'.HTTP.'/admin/');
+define('HTTPS_IMAGE', 'https://'.HTTP.'/image/');
 
 // DIR
 define('BASE_DIR', str_replace(DIRECTORY_SEPARATOR.'admin', '', realpath(dirname(__FILE__))));
 define('DIR_APPLICATION', BASE_DIR.'/admin/');
 define('DIR_SYSTEM', BASE_DIR.'/system/');
-define('DIR_DATABASE', BASE_DIR.'/system/database/');
 define('DIR_LANGUAGE', BASE_DIR.'/admin/language/');
 define('DIR_TEMPLATE', BASE_DIR.'/admin/view/template/');
 define('DIR_CONFIG', BASE_DIR.'/system/config/');
 define('DIR_IMAGE', BASE_DIR.'/image/');
 define('DIR_CACHE', BASE_DIR.'/system/cache/');
 define('DIR_DOWNLOAD', BASE_DIR.'/download/');
+define('DIR_UPLOAD', BASE_DIR.'/system/upload/');
 define('DIR_LOGS', BASE_DIR.'/system/logs/');
+define('DIR_MODIFICATION', BASE_DIR.'/system/modification/');
 define('DIR_CATALOG', BASE_DIR.'/catalog/');
 
 // DB
--- config.php
+++ config.php
@@ -5,19 +5,20 @@ define('HTTP', $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/');
 define('HTTP_SERVER', 'http://'.HTTP);
 
 // HTTPS
-define('HTTPS_SERVER', 'http://'.HTTP);
+define('HTTPS_SERVER', 'https://'.HTTP);
 
 // DIR
 define('BASE_DIR', realpath(dirname(__FILE__)));
 define('DIR_APPLICATION', BASE_DIR.'/catalog/');
 define('DIR_SYSTEM', BASE_DIR.'/system/');
-define('DIR_DATABASE', BASE_DIR.'/system/database/');
 define('DIR_LANGUAGE', BASE_DIR.'/catalog/language/');
 define('DIR_TEMPLATE', BASE_DIR.'/catalog/view/theme/');
 define('DIR_CONFIG', BASE_DIR.'/system/config/');
 define('DIR_IMAGE', BASE_DIR.'/image/');
 define('DIR_CACHE', BASE_DIR.'/system/cache/');
 define('DIR_DOWNLOAD', BASE_DIR.'/download/');
+define('DIR_UPLOAD', BASE_DIR.'/system/upload/');
+define('DIR_MODIFICATION', BASE_DIR.'/system/modification/');
 define('DIR_LOGS', BASE_DIR.'/system/logs/');
 
 // DB
