f64d5dd5e0cccb88eb4503753fa8cefec40332a4
Bommer <quangthi_90@yahoo.com.vn>
2015-01-14 21:35:16 +0700

    - Apply data for list all products
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- catalog/controller/product/categories.php
+++ catalog/controller/product/categories.php
@@ -1,6 +1,58 @@
 <?php
 class ControllerProductCategories extends Controller {
 	public function index() {
+		$this->load->model('catalog/category');
+		$this->load->model('catalog/product');
+		$this->load->model('tool/image');
+
+		$data['categories'] = array();
+		$data['products'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories as $category) {
+			$filter_data = array(
+				'filter_category_id' => $category['category_id'],
+				'start'				 => 0,
+				'limit'              => 1000
+			);
+
+			$data['products'][$category['category_id']] = array();
+			
+			$products = $this->model_catalog_product->getProducts($filter_data);
+
+			foreach ($products as $product) {
+				if ($product['image']) {
+					$image = $this->model_tool_image->resize($product['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+				}
+
+				if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+					$price = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')));
+				} else {
+					$price = false;
+				}
+
+				$data['products'][$category['category_id']][] = array(
+					'product_id'  => $product['product_id'],
+					'thumb'       => $image,
+					'name'        => $product['name'],
+					'price'       => $price,
+					'href'        => $this->url->link('product/product', 'product_id=' . $product['product_id'])
+				);
+			}
+
+			if (count($products) > 0) {
+				$data['categories'][] = array(
+					'id' 	   => $category['category_id'],
+					'name'     => $category['name'],
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+				);
+			}
+		}
+
+		$data['button_cart'] = $this->language->get('button_cart');
 
 		$data['content_top'] = $this->load->controller('common/content_top');
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
--- catalog/view/theme/default/template/product/categories.tpl
+++ catalog/view/theme/default/template/product/categories.tpl
@@ -9,25 +9,25 @@
     <div class="content-up" id="content-up"></div>
     <div id="content-mid">
       <?php echo $content_top; ?>
-      <?php for($j = 0; $j < 4; $j ++) { ?>
-      <h2 class="heading-title">Category <?php echo ($j + 1); ?></h2>
+      <?php foreach($categories as $category) { ?>
+      <h2 class="heading-title"><a href="<?php echo $category['href']; ?>"><?php echo $category['name']; ?></a></h2>
       <div class="break"></div>
       <div class="product-list">
-        <?php for($i = 0; $i < 8; $i ++) { ?>
+        <?php foreach($products[$category['id']] as $product) { ?>
         <div class="product-item">
           <div class="image">
-            <a href="/" class="detail-link">
-              <img src="image/no_image.png" alt="Product Name" title="Product Name" class="img-responsive" />
+            <a href="<?php echo $product['href']; ?>" class="detail-link">
+              <img src="<?php echo $product['thumb']; ?>" alt="<?php echo $product['name']; ?>" title="<?php echo $product['name']; ?>" class="img-responsive" />
             </a>
           </div>
           <div class="caption">
-            <h4 class="product-heading" title="Product Name"><a href="/">Product Name <?php echo ($i + 1); ?></a></h4>
+            <h4 class="product-heading" title="<?php echo $product['name']; ?>"><a href="<?php echo $product['href']; ?>"><?php echo $product['name']; ?></a></h4>
           </div>
           <p class="text-center price">
-            1,333.222 VND
+            <?php echo $product['price']; ?>
           </p>
           <div class="actions">
-            <button type="button" class="btn btn-info btn-sm"><i class="fa fa-plus"></i> <span class="hidden-xs hidden-sm hidden-md">Add to cart</span></button>
+            <button type="button" class="btn btn-info btn-sm" onclick="cart.add('<?php echo $product['product_id']; ?>');"><i class="fa fa-plus"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $button_cart; ?></span></button>
           </div>
         </div>
         <?php } ?>
