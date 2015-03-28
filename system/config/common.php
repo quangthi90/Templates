2d864244e57129abdd5518d6b54f14b41d7ac466
Bommer <quangthi_90@yahoo.com.vn>
2015-03-28 14:21:18 +0700

    - Update banner fixed link
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- system/config/common.php
+++ system/config/common.php
@@ -2,6 +2,6 @@
 
 $_['common']['affiliate']['remove_news_id'] 	= '3';
 
-$_['common']['banners']['left_bottom'] 			= '<img src="image/catalog/logo.png" />';
+$_['common']['banners']['left_bottom'] 			= '<img src="image/catalog/banners/fixed_banner.png" />';
 
 ?>
