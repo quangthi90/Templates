a5d2842eeda4d6770326258c35a02d7950692f51
Bommer <quangthi_90@yahoo.com.vn>
2015-05-22 18:45:38 +0700

    - Add config file
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/safefood/admin/');
+define('HTTP_CATALOG', 'http://localhost/safefood/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/safefood/admin/');
+define('HTTPS_CATALOG', 'http://localhost/safefood/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Thiet_web\xamp\htdocs\safefood/admin/');
+define('DIR_SYSTEM', 'D:\Thiet_web\xamp\htdocs\safefood/system/');
+define('DIR_LANGUAGE', 'D:\Thiet_web\xamp\htdocs\safefood/admin/language/');
+define('DIR_TEMPLATE', 'D:\Thiet_web\xamp\htdocs\safefood/admin/view/template/');
+define('DIR_CONFIG', 'D:\Thiet_web\xamp\htdocs\safefood/system/config/');
+define('DIR_IMAGE', 'D:\Thiet_web\xamp\htdocs\safefood/image/');
+define('DIR_CACHE', 'D:\Thiet_web\xamp\htdocs\safefood/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Thiet_web\xamp\htdocs\safefood/system/download/');
+define('DIR_UPLOAD', 'D:\Thiet_web\xamp\htdocs\safefood/system/upload/');
+define('DIR_LOGS', 'D:\Thiet_web\xamp\htdocs\safefood/system/logs/');
+define('DIR_MODIFICATION', 'D:\Thiet_web\xamp\htdocs\safefood/system/modification/');
+define('DIR_CATALOG', 'D:\Thiet_web\xamp\htdocs\safefood/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'safefood');
+define('DB_PREFIX', 'sf_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/safefood/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/safefood/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Thiet_web\xamp\htdocs\safefood/catalog/');
+define('DIR_SYSTEM', 'D:\Thiet_web\xamp\htdocs\safefood/system/');
+define('DIR_LANGUAGE', 'D:\Thiet_web\xamp\htdocs\safefood/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Thiet_web\xamp\htdocs\safefood/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Thiet_web\xamp\htdocs\safefood/system/config/');
+define('DIR_IMAGE', 'D:\Thiet_web\xamp\htdocs\safefood/image/');
+define('DIR_CACHE', 'D:\Thiet_web\xamp\htdocs\safefood/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Thiet_web\xamp\htdocs\safefood/system/download/');
+define('DIR_UPLOAD', 'D:\Thiet_web\xamp\htdocs\safefood/system/upload/');
+define('DIR_MODIFICATION', 'D:\Thiet_web\xamp\htdocs\safefood/system/modification/');
+define('DIR_LOGS', 'D:\Thiet_web\xamp\htdocs\safefood/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'safefood');
+define('DB_PREFIX', 'sf_');
